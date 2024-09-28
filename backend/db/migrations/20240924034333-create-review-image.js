1907a2b0f496ccbde03854eb97a7728b86cb3798
Tucker VandenBos <tuckervbos@gmail.com>
2024-09-28 15:38:13 -0500

    more options bug

--- backend/db/migrations/20240924034333-create-review-image.js
+++ backend/db/migrations/20240924034333-create-review-image.js
@@ -8,34 +8,38 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable("ReviewImages", {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
+		await queryInterface.createTable(
+			"ReviewImages",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				reviewId: {
+					type: Sequelize.INTEGER,
+					allowNull: false,
+					references: { model: "Reviews", key: "id" },
+					onDelete: "CASCADE",
+				},
+				url: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+				},
 			},
-			reviewId: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				references: { model: "Reviews", key: "id" },
-				onDelete: "CASCADE",
-			},
-			url: {
-				type: Sequelize.STRING,
-				allowNull: false,
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-			},
-		});
+			options
+		);
 	},
 	async down(queryInterface, Sequelize) {
 		options.tableName = "ReviewImages";
