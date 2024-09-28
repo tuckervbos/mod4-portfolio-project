6a6b08ca7dfd64c797261478175d68661a0413db
Tucker VandenBos <tuckervbos@gmail.com>
2024-09-28 14:32:41 -0500

    bug

--- backend/app.js
+++ backend/app.js
@@ -55,8 +55,6 @@ app.use((_req, _res, next) => {
 	next(err);
 });
 
-const { ValidationError } = require("sequelize");
-
 // Process sequelize errors
 app.use((err, _req, _res, next) => {
 	// check if error is a Sequelize error:
