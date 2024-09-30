fa72135636e4e4ad38b6699203e43e2e7ac02cdc
Tucker VandenBos <tuckervbos@gmail.com>
2024-09-30 04:14:19 -0500

    booking bug

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -12,7 +12,6 @@ const { Sequelize } = require("sequelize");
 const { check, query, validationResult } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const { Op } = require("sequelize");
-const { invalidateDates, conflictExists } = require("./bookings");
 
 const validateSpot = [
 	check("name").exists({ checkFalsy: true }).withMessage("Name is required"),
@@ -425,6 +424,66 @@ router.get("/", validateQueryParams, async (req, res, next) => {
 });
 
 //> create a review for a spot based on spot's id || POST /api/spots/:spotId/reviews
+
+// ! Helper function to check for conflicts
+
+const conflictExists = (startDateConflict, endDateConflict) => {
+	const conflictErrors = {};
+	if (startDateConflict) {
+		conflictErrors.startDate = "Start date conflicts with an existing booking";
+	}
+	if (endDateConflict) {
+		conflictErrors.endDate = "End date conflicts with an existing booking";
+	}
+	if (Object.keys(conflictErrors).length > 0) {
+		res.status(403).json({
+			message: "Sorry, this spot is already booked for the specified dates",
+			errors: conflictErrors,
+		});
+		return true;
+	}
+	return false;
+};
+
+//!  Helper function to invalidate dates
+
+const invalidateDates = (startDate, endDate, res) => {
+	const errors = {};
+	const today = new Date();
+	const start = new Date(startDate);
+	const end = new Date(endDate);
+
+	if (!startDate || isNaN(start)) {
+		errors.startDate = "Invalid or missing startDate";
+	}
+	if (!endDate || isNaN(end)) {
+		errors.endDate = "Invalid or missing endDate";
+	}
+
+	if (Object.keys(errors).length > 0) {
+		res.status(400).json({
+			message: "Bad Request",
+			errors,
+		});
+		return true;
+	}
+
+	if (start < today) {
+		errors.startDate = "startDate cannot be in the past";
+	}
+	if (end <= start) {
+		errors.endDate = "endDate cannot be on or before startDate";
+	}
+
+	if (Object.keys(errors).length > 0) {
+		res.status(400).json({
+			message: "Bad Request",
+			errors,
+		});
+		return true;
+	}
+	return false;
+};
 
 router.post(
 	"/:spotId/reviews",
