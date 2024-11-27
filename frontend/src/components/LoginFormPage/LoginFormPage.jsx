67f5869b6b2937adb119af95dda388a98350f0e9
Tucker VandenBos <tuckervbos@gmail.com>
2024-11-27 07:17:02 -0600

    login form component functioning

--- frontend/src/components/LoginFormPage/LoginFormPage.jsx
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,30 +1,36 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import "./LoginForm.css";
 
 function LoginFormPage() {
 	const dispatch = useDispatch();
 	const sessionUser = useSelector((state) => state.session.user);
+
+	// controlled inputs and error state:
 	const [credential, setCredential] = useState("");
 	const [password, setPassword] = useState("");
 	const [errors, setErrors] = useState({});
 
+	// redirect if user is already logged in
 	if (sessionUser) return <Navigate to="/" replace={true} />;
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		setErrors({});
+		setErrors({}); // clear errors before attempting to login
+
+		// dispatch login thunk:
 		return dispatch(sessionActions.login({ credential, password })).catch(
 			async (res) => {
 				const data = await res.json();
-				if (data?.errors) setErrors(data.errors);
+				if (data?.errors) setErrors(data.errors); // set errors from the response
 			}
 		);
 	};
 
 	return (
-		<>
+		<div className="login-form-container">
 			<h1>Log In</h1>
 			<form onSubmit={handleSubmit}>
 				<label>
@@ -48,7 +54,7 @@ function LoginFormPage() {
 				{errors.credential && <p>{errors.credential}</p>}
 				<button type="submit">Log In</button>
 			</form>
-		</>
+		</div>
 	);
 }
 
